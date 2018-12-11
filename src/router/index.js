a1190e089750d40f5e313e756f42f623111f8476
Griffin <griffinmuteti31@gmail.com>
2018-12-11 09:08:31 +0300

    inconsistency flow
    - update functionality for marking inconsistency
    -  inconsistency form to show on mark inconsistency
    - move css separate css file
    - update iconsistency detail routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,78 +1,76 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import login from '@/components/login'
-import applications_list from '@/components/applications/list'
-import applicant_detail from '@/components/applications/details'
+import Vue from "vue";
+import Router from "vue-router";
+import login from "@/components/login";
+import applications_list from "@/components/applications/list";
+import applicant_detail from "@/components/applications/details";
 
-import reviewed_list from '@/components/reviewed/list'
-import renewals_list from '@/components/renewals/list'
+import reviewed_list from "@/components/reviewed/list";
+import renewals_list from "@/components/renewals/list";
 
-import reviewed_detail from '@/components/reviewed/details'
+import reviewed_detail from "@/components/reviewed/details";
 
-import inconsistencies_list from '@/components/inconsistencies/list'
-import inconsistencies_details from '@/components/inconsistencies/details'
+import inconsistencies_list from "@/components/inconsistencies/list";
+import inconsistencies_details from "@/components/inconsistencies/details";
 
-
-Vue.use(Router)
+Vue.use(Router);
 
 let router = new Router({
-    routes: [
-        {
-            path: '/',
-            name: 'login',
-            component: login
-        },
-        {
-            path: '/applications',
-            name: 'applications',
-            component: applications_list
-        },
-        {
-            path: '/applicant/:id',
-            name: 'applicant',
-            component: applicant_detail
-        },
-        {
-            path: '/reviewed',
-            name: 'reviewed',
-            component: reviewed_list
-        },
-        {
-            path: '/reviewed-applicant/:id',
-            name: 'reviewed-applicant',
-            component: reviewed_detail
-
-        },
-        {
-            path: '/renewals',
-            name: 'renewals',
-            component: renewals_list
-        },
-        {
-            path: '/inconsistencies',
-            name: 'inconsistencies',
-            component: inconsistencies_list
-        },
-        {
-            path: '/applicant-inconsistencies/:id',
-            name: 'inconsistencies-details',
-            component: inconsistencies_details
-        },
-
-    ]
-})
+  routes: [
+    {
+      path: "/",
+      name: "login",
+      component: login
+    },
+    {
+      path: "/applications",
+      name: "applications",
+      component: applications_list
+    },
+    {
+      path: "/applicant/:id",
+      name: "applicant",
+      component: applicant_detail
+    },
+    {
+      path: "/reviewed",
+      name: "reviewed",
+      component: reviewed_list
+    },
+    {
+      path: "/reviewed-applicant/:id",
+      name: "reviewed-applicant",
+      component: reviewed_detail
+    },
+    {
+      path: "/renewals",
+      name: "renewals",
+      component: renewals_list
+    },
+    {
+      path: "/inconsistencies",
+      name: "inconsistencies",
+      component: inconsistencies_list
+    },
+    {
+      path: "/inconsistency/:id",
+      name: "inconsistencies-details",
+      component: inconsistencies_details
+    }
+  ]
+});
 
 router.beforeEach((to, from, next) => {
-    if (to.path == '/') {
-        if (!localStorage.authenticated || !JSON.parse(localStorage.authenticated)) next();
-        else next('/applications');
-        return;
-    }
-    if (localStorage.authenticated) {
-        JSON.parse(localStorage.authenticated) ? next() : next('/');
-    } else {
-        next('/');
-    }
-})
+  if (to.path == "/") {
+    if (!localStorage.authenticated || !JSON.parse(localStorage.authenticated))
+      next();
+    else next("/applications");
+    return;
+  }
+  if (localStorage.authenticated) {
+    JSON.parse(localStorage.authenticated) ? next() : next("/");
+  } else {
+    next("/");
+  }
+});
 
 export default router;
