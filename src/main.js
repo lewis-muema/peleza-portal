32f14701b81ebeff8e7a674bf417384153923b06
joyce.k <joyce@sendy.co.ke>
2020-08-17 16:59:33 +0300

    set icon path

--- src/main.js
+++ src/main.js
@@ -17,9 +17,8 @@ import axios from 'axios';
 import moment from 'moment';
 
 import ElementUI from 'element-ui';
-import 'element-theme-default';
 import locale from 'element-ui/lib/locale/lang/en';
-import 'element-ui/lib/theme-chalk/index.css';
+import './styles.scss';
 
 import App from './App';
 import router from './router';
