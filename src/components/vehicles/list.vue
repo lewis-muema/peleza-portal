a3e25d207826e8519ee7e52bcd6c556b13aba29f
joyce.k <joyce@sendy.co.ke>
2020-09-02 14:25:47 +0300

    introduce vehicles tab

---
+++ src/components/vehicles/list.vue
@@ -0,0 +1,18 @@
+<template xmlns:router-link="">
+  <div class="stageone">
+      <applicantList :category="category" />
+  </div>
+</template>
+<script>
+import applicantList from '../shared/applicantList.vue';
+
+export default {
+    name: 'PendingList',
+    components: { applicantList },
+    data() {
+        return {
+            category: 'vehicles',
+        };
+    },
+};
+</script>
