67ff08602f5c1e6581bee5634ec54d1af6b26306
shelef ndar <shelef@sendy.co.ke>
2020-11-09 21:05:00 +0300

    APM url updates

--- config/dev.env.js
+++ config/dev.env.js
@@ -8,4 +8,5 @@ module.exports = merge(prodEnv, {
   AUTH_URL: '"https://authtest.sendyit.com/"',
   BASE_URL: '"https://ridertest.sendyit.com/admin_partner_api/v5/admin/"',
   PARTNER_BASE_URL: '"https://ridertest.sendyit.com/admin_partner_api/v5/"',
+  ELASTIC_APM_SERVER_URL: '"https://staging-elk.sendyit.com:8200"',
 });
--- config/test.env.js
+++ config/test.env.js
@@ -7,4 +7,5 @@ module.exports = {
   PARTNER_BASE_URL: '"https://ridertest.sendyit.com/admin_partner_api/v5/"',
   NODE_PARTNER_API: '"https://ridertest.sendyit.com/node_partner_api/v1/"',
   NODE_API_SWITCH: false,
+  ELASTIC_APM_SERVER_URL: '"https://staging-elk.sendyit.com:8200"',
 };
