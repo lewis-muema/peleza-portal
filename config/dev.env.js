8ff9a91439877ec64497306531fc12c0d510733d
joyce.k <joyce@sendy.co.ke>
2020-12-15 15:21:25 +0300

    search customers from sor

--- config/dev.env.js
+++ config/dev.env.js
@@ -12,4 +12,11 @@ module.exports = merge(prodEnv, {
   ELASTIC_APM_DISTRIBUTED_TRACING_ORIGINS: '"https://authtest.sendyit.com"',
   ELASTIC_APM_SERVER_URL: '"https://staging-elk.sendyit.com:8200"',
   ELASTIC_APM_ENVIRONMENT: '"development"',
+  VUE_APP_SOLR_BASE: '"https://gate.sendyit.com/solr/"',
+  VUE_APP_SOLR_OWNERS: '"ownerstaging/"',
+  VUE_APP_SOLR_VEHICLES: '"vehiclestaging/"',
+  VUE_APP_SOLR_COP_USERS: '"cop_clientsstaging/"',
+  VUE_APP_SOLR_PEER_USERS: '"peer_clientsstaging/"',
+  VUE_APP_SOLR_JWT: '"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJyV01HZVR2WWZMVnlBSWwxOHFPVGFWMnRxMnFDVmpJZiIsIm5hbWUiOiJzb2xyIn0.p7uW30OQBaSEduNerbIaSbaQTdUAa-VkVMQUF4LAPFQ"',
+
 });
